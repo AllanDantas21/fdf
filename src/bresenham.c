74bfd48af6fbc668dd399de5ac6116a02302de18
AllanDantas21 <Allandantas21.ad@gmail.com>
2024-01-26 23:26:45 -0300

    refact bresenham

--- src/bresenham.c
+++ src/bresenham.c
@@ -17,55 +17,39 @@ static void adjust_zoom(int *x1, int *y1, int *x2, int *y2, fdf *data)
 void bresenham(int x1, int y1, int x2, int y2, int color, fdf *data)
 {
     adjust_zoom(&x1, &y1, &x2, &y2, data);
+
     int dx = x2 - x1;
     int dy = y2 - y1;
-
-    int d = 2 * dy - dx; // valor inicial de d
-
-    int incE = 2 * dy;    // incremento para mover E
-    int incNE = 2 * (dy - dx); // incremento para mover NE
-
+    int d = 2 * dy - dx;
+    int incE = 2 * dy;
+    int incNE = 2 * (dy - dx);
     int x = x1;
     int y = y1;
+
     put_pixel(data, x, y, color);
 
-    // Lidar com linhas verticais
-    if (dx == 0)
-    {
-        while (y < y2)
-        {
+    if (dx == 0) {
+        while (y < y2) {
             y++;
             put_pixel(data, x, y, color);
         }
-        return;
-    }
-
-    // Lidar com linhas horizontais
-    if (dy == 0)
-    {
-        while (x < x2)
-        {
+    } else if (dy == 0) {
+        while (x < x2) {
             x++;
             put_pixel(data, x, y, color);
         }
-        return;
-    }
-
-    // Loop principal
-    while (x != x2 || y != y2)
-    {
-        if (d <= 0)
-        {
-            d += incE;
-            x++;
+    } else {
+        while (x != x2 || y != y2) {
+            if (d <= 0) {
+                d += incE;
+                x++;
+            } else {
+                d += incNE;
+                x++;
+                y++;
+            }
+            put_pixel(data, x, y, color);
         }
-        else
-        {
-            d += incNE;
-            x++;
-            y++;
-        }
-        put_pixel(data, x, y, color);
     }
 }
 
